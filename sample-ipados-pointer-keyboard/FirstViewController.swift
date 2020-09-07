5fc1f1a4575aa7f66d40bc2e13e4db6a9ba35351
cc4966 <cc4966@gmail.com>
2020-09-07 23:36:12 +0900

    Initial Commit

---
+++ sample-ipados-pointer-keyboard/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  sample-ipados-pointer-keyboard
+//
+//  Created by Ryo Nonaka on 2020/09/07.
+//  Copyright © 2020 Ryo Nonaka. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

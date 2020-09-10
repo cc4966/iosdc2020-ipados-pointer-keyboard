208bfdb6efa1fb2a3f9a14aaecfdd7fe4f3efb31
cc4966 <cc4966@gmail.com>
2020-09-10 13:36:48 +0900

    プレビュー用の画面をポインター確認画面も兼ねるように変更

--- sample-ipados-pointer-keyboard/PreviewViewController.swift
+++ sample-ipados-pointer-keyboard/PreviewViewController.swift
@@ -10,9 +10,32 @@ import UIKit
 
 class PreviewViewController: UIViewController {
 
+    private var constraintY: NSLayoutConstraint?
+    private var constraintX: NSLayoutConstraint?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        let textField = UITextField()
+        textField.borderStyle = .roundedRect
+        textField.text = "テキスト入力欄"
+        textField.font = .systemFont(ofSize: 36)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(textField)
+        view.backgroundColor = .systemBackground
+
+        textField.centerYAnchor.constraint(equalToSystemSpacingBelow: view.centerYAnchor, multiplier: 1).isActive = true
+        textField.centerXAnchor.constraint(equalToSystemSpacingAfter: view.centerXAnchor, multiplier: 1).isActive = true
+        textField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1, constant: -160).isActive = true
+        updateViewConstraints()
+
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(title: "test", style: .plain, target: self, action: #selector(barButtonItem(_:))),
+            UIBarButtonItem(image: .strokedCheckmark, style: .plain, target: self, action: #selector(barButtonItem(_:)))
+        ]
+    }
+
+    @objc func barButtonItem(_ sender: UIBarButtonItem) {
+        // none
     }
 
 }
